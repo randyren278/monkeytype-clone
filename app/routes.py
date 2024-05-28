f1ebd8b3ef2e3c0621f948979612b7d8c882070f
randyren278 <randyren278@gmail.com>
2024-05-28 19:55:55 +0200

    "Removed pycache files, updated routes.py, and deleted tempCodeRunnerFile.py"

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
+print("Registering route...")
 
 @main.route('/')
 def index():
+    print("Rendering index...")
     return render_template('index.html')
